4f3bffb6f4b8b9225cb49739c99d2880e237e828
Charles Dias <charlesdias.cd@outlook.com>
2024-01-15 17:10:12 -0300

    fea: improve the sanitizer_demo function

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -167,6 +167,15 @@ void sanitizer_demo(void)
     int *p = new int[10];
     p[0] = 0;
     p = new int[10];
+    p[10] = 1;
+
+    std::vector<int> v = { 1, 2, 3};
+    auto two = find(v.begin(), v.end(), 2);
+    for(int i = 0; i < 4096; i++)
+    {
+        v.push_back(i);
+    }
+    std::cout << *two << std::endl;
 }
 
 void cxxopts_demo(int argc, char **argv)
